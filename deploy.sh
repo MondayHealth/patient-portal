8a1b364d97fbd422a56411742261d44b3594246b
ixtli <chris.galardi@risingtidegames.com>
2018-03-29 13:16:21 -0400

    service worker caching

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ CL_DIST_ID=E13U00LONPLKVG
 npm run build
 cd build
 aws s3 sync . s3://patient.monday.health --delete --acl public-read
+aws s3 sync ./service-worker.js s3://patient.monday.health/service-worker.js \
+    --acl public-read \
+    --metadata-directive REPLACE \
+    --cache-control max-age=0,no-cache,no-store,must-revalidate
 
 echo ""
 echo "Invalidating CloudFront. If this fails, you likely didnt wait long enough"
@@ -12,5 +16,5 @@ echo "after the last one. It needs not to be 'In Progress'."
 echo ""
 
 aws cloudfront create-invalidation --distribution-id ${CL_DIST_ID} \
-  --paths / /index.html
+  --paths / /index.html /service-worker.js
 
