cafdaa44f5ea82d81feb24619428b829085df2ba
ixtli <chris.galardi@risingtidegames.com>
2018-03-29 13:36:24 -0400

    handle service worker caching correctly

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,15 @@ CL_DIST_ID=E13U00LONPLKVG
 
 npm run build
 cd build
-aws s3 sync . s3://patient.monday.health --delete --acl public-read
-aws s3 sync ./service-worker.js s3://patient.monday.health/service-worker.js \
+
+echo ""
+echo "Uploading build..."
+aws s3 sync . s3://patient.monday.health --delete \
+    --acl public-read \
+    --exclude service-worker.js
+
+echo "Uploading service-worker.js"
+aws s3 cp ./service-worker.js s3://patient.monday.health/service-worker.js \
     --acl public-read \
     --metadata-directive REPLACE \
     --cache-control max-age=0,no-cache,no-store,must-revalidate
